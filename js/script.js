f15005427ac126124476069f4690b8b27457917e
Abdulaziz <sinfsinf74@gmail.com>
2023-01-16 12:36:46 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+let namePropmt = prompt('Ismingiz nima?')
+let surnamePropmt = prompt('familiyangiz nima?')
+let agePropmt = prompt('Yoshingiz nechchida?')
+let user = {}
+
+user.name = namePropmt
+user.surname = surnamePropmt
+user.age = agePropmt
+
+
+console.log(namePropmt)
+console.log(user.surname)
+console.log(user.age)
